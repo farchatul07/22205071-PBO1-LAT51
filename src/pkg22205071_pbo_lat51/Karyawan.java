54bb179b24b4c57fc590c891084b571ab5a6e2ca
farchatul07 <145920038+farchatul07@users.noreply.github.com>
2023-11-29 15:15:13 +0700

    Update Karyawan.java

--- src/pkg22205071_pbo_lat51/Karyawan.java
+++ src/pkg22205071_pbo_lat51/Karyawan.java
@@ -8,6 +8,11 @@ import java.util.Scanner;
 /**
  *
  * @author user
+ Nama              : Farchatul Hudayah
+ Nim               : 22205071
+ Prodi             : Teknik Informatika
+ Kelas             : PBO1
+ Deskripsi Program : Program ini berisi program untuk Menampilkan GAJI KARYAWAN
  */
 class Karyawan {
 
